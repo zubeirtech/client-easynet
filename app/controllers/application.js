79043d9976e9d2f6e6d7307d790272b46bbc0660
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-28 04:04:48 +0200

    add sign up route

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,7 +2,11 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-    session: service()
+    session: service(),
 
-    
+    actions: {
+        invalidateSession() {
+            this.get('session').invalidate();
+        }
+    }
 });
--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('login');
   this.route('feed');
+  this.route('sign-up');
 });
 
 export default Router;
