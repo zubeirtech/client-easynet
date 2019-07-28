76e7d12194a1f454229e5b32007b2e6928c17781
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-28 02:16:45 +0200

    Make footer and customize ui

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,8 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+    session: service()
+
+    
 });
--- app/router.js
+++ app/router.js
@@ -7,7 +7,8 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('feeds');
+  this.route('login');
+  this.route('feed');
 });
 
 export default Router;
