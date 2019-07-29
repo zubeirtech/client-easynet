fa21a120368f9d1f0ef7fb7382c958dacd2fccf3
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-29 18:12:57 +0200

    add route after authentication

--- config/environment.js
+++ config/environment.js
@@ -23,6 +23,11 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['ember-simple-auth'] = {
+    routeAfterAuthentication: '/feed',
+    resourceName: 'social',
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
