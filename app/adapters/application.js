a83191ec1d500d344a103a49887cbf1b64bef30b
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-30 22:53:17 +0200

    fixing google auth problem

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,18 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import { inject as service } from '@ember/service';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-    host: 'http://localhost:5000'
+    host: 'http://localhost:5000',
+    session: service('session'),
+
+    authorize(xhr) {
+        if (this.session.access_token) {
+            let { access_token } = this.session;
+            xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+        } else {
+            let { access_token } = this.get('session.data.authenticated');
+            xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+        }
+    },
 });
--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -1,11 +1,18 @@
 import ToriiAuthenticator from 'ember-simple-auth/authenticators/torii';
 import { inject as service } from '@ember/service';
-import { set } from '@ember/object';
+import { set, computed } from '@ember/object';
 
 export default ToriiAuthenticator.extend({
     torii: service(),
     session: service(),
     ajax: service(),
+    serverTokenEndpoint: 'http://localhost:5000/auth-google',
+    access_token: computed('this.session', function () {
+        return this.session.access_token;
+    }),
+
+    async restore(data) {
+    },
 
     async authenticate(provider, options) {
         try {
@@ -17,9 +24,17 @@ export default ToriiAuthenticator.extend({
                     redirect_uri: authResponse.redirectUri
                 },
             });
-            
+            const { access_token } = res;
+            set(this.session.data.authenticated, 'access_token', access_token);
+            return access_token;
         } catch (error) {
+            document.location.reload();
             console.log(error);
         }
+    },
+
+    async invalidate(data) {
+
     }
+
 });
