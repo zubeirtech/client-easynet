45b7961564c1190f9e881ecc3ed42d14f18b829a
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-04 19:44:01 +0200

    Fix bug

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,4 @@ import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Route.extend(ApplicationRouteMixin, {
-    beforeModel() {
-        this.transitionTo('feed');
-    }
 });
