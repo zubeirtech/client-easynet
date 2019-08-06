9f921e6c7e925c9cc98b1045d82afa07cf9d8935
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-06 03:41:24 +0200

    Direct to right token endpoint

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,5 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-    serverTokenEndpoint: 'http://localhost:5000/token',
+    serverTokenEndpoint: 'https://api-easynet.herokuapp.com/token',
 });
