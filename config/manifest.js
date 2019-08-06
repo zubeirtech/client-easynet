c1e5c70f54890f3a04da8faf536527161042325a
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-06 02:34:44 +0200

    Setup PWA configuration

---
+++ config/manifest.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: "client",
+    short_name: "client",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "https://imgur.com/HrD1F2p.png",
+        sizes: "540x540",
+        type: "image/png"
+      }
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
